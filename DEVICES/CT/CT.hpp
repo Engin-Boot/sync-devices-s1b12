37ef72e6662dbb5ada2a11ba09a1c74b04f8b39f
Shivaprasad <shivaprasadsmys@gmail.com>
2020-10-16 01:39:09 +0530

    created CT and ULTRASOUND clients and moved them to DEVICES

---
+++ DEVICES/CT/CT.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <unordered_map>
+#include <stack>
+#include "../../Patient/Patient.hpp"
+#include "pub_sub_utils.hpp"
+using namespace std;
+
+typedef void (*ScriptFunction)(void); // function pointer type
+typedef unordered_map<int, ScriptFunction> script_map;
+
+extern stack<Patient> patient_stack;
+
+Patient get_patient_information();
+Patient pop_and_return_patient();
+Patient UnFlatten(const string& str);
+void display_main_menu();
+void display_edit_menu();
+void add_patient();
+void view_current_patient_data();
+void edit_name();
+void edit_age();
+void edit_gender();
+void edit_procedure();
+void edit_consumables_used();
+void display_data(Patient& p);
+void view_patient_count();
+void edit_all();
+void call_script_corresponding_to_choice(script_map& m, const int choice);
+void edit_current_patient_data();
+void serialize_and_publish(MQTTAsync& client, MQTTAsync_responseOptions& pub_opts);
+void repeat_until_finished(MQTTAsync& client, MQTTAsync_responseOptions& pub_opts);
+void wait_for_disconnection(MQTTAsync& client, MQTTAsync_disconnectOptions& disc_opts);
+void init_globals();
---
+++ DEVICES/CT/CT_MAIN.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_map>
+#include "pub_sub_utils.hpp"
+#include "../../Patient/Patient.hpp"
+#include "CT.hpp"
+
+using namespace std;
+
+// Global variables shared across multiple translation units
+pubsub_opts opts;
+int finished;
+int subscribed;
+int toStop;
+int disconnected;
+stack<Patient> patient_stack;
+
+// Global variables visible to current translation unit only
+
+
+int main(int argc, char** argv)
+{
+    // Initial configurations and setup
+	MQTTAsync_disconnectOptions disc_opts = MQTTAsync_disconnectOptions_initializer;
+	MQTTAsync_createOptions create_opts = MQTTAsync_createOptions_initializer;
+    MQTTAsync_responseOptions pub_opts = MQTTAsync_responseOptions_initializer;
+	MQTTAsync client;
+
+    // Initialize global variables
+    init_globals();
+
+    // MQTT Broker url
+    char* url = (char*)BROKER_URL;
+
+    // Create Client Object
+    create_client_object(client, create_opts, url);
+
+    // Set Callbacks
+    set_callbacks(client);
+    
+    // Connect client to broker
+	myconnect(client);
+
+    // Set Publish Callbacks
+    pub_opts.onSuccess = onPublish;
+	pub_opts.onFailure = onPublishFailure;
+
+    //Repeat until finished:
+    // Publish any messages the client needs to
+    // Handle any incoming messages 
+    while (!toStop)
+	{
+        sleep(5);
+        display_main_menu();
+        repeat_until_finished(client, pub_opts);
+        sleep(5);
+	}
+
+    // Disconnect
+	wait_for_disconnection(client, disc_opts);
+
+    // Destroy client
+	MQTTAsync_destroy(&client);
+
+	return EXIT_SUCCESS;
+}

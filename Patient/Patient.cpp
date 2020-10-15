7801ce1bb2d9975210bb36c690eb9c683f51ecf9
haneef15 <haneefsk2204@gmail.com>
2020-10-15 21:11:08 +0530

    Changed gender from char to string

--- Patient/Patient.cpp
+++ Patient/Patient.cpp
@@ -3,61 +3,60 @@
 #include <sstream>
 #include <vector>
 #include "Patient.hpp"
-
+ 
 string Patient::toString()
 {
-    return name + "|" + to_string(this->age)+ "|"+to_string(this->gender)+ "|" +procedure+ "|" +consumables_used;
+    return name + "|" + to_string(this->age)+ "|" + this->gender + "|" + procedure + "|" + consumables_used;
 }
-
+ 
 void Patient::setName(const string& name)
 {
     this->name = name;
 }
-
+ 
 string Patient :: getName()
 {
     return this->name;
 }
-
+ 
 void Patient::setAge(int age)
 {
     this->age = age;
 }
-
-void Patient::setGender(char gender)
+ 
+void Patient::setGender(const string& gender)
 {
     this->gender = gender;
 }
-
+ 
 void Patient:: setProcedure(const string& procedure)
 {
     this->procedure = procedure;
 }
-
+ 
 void Patient:: setConsumables_used(const string& consumables_used)
 {
     this->consumables_used = consumables_used;
 }
 
 
-
+ 
 int Patient :: getAge()
 {
     return this->age;
 }
-
-char Patient :: getGender()
+ 
+string Patient :: getGender()
 {
     return this->gender;
 }
-
+ 
 string Patient :: getProcedure()
 {
     return this->procedure;
 }
-
+ 
 string Patient :: getConsumables_used()
 {
     return this->consumables_used;
 }
- 
